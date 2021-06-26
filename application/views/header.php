457cfb8c863a2c8e94aafeea7008d32a5a2b368f
KholiqAmrulloh <kholiqamrulloh24@gmail.com>
2021-06-26 19:01:23 +0700

    template tabel

---
+++ application/views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <link href="<?php echo base_url('vendor/sbadmin2/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('vendor/sbadmin2/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="<?php echo base_url('vendor/sbadmin2/vendor/datatables/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
+    <title>TubesTekWeb</title>
+</head>
+
+<body>
